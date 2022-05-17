afabd7fd482174c0a28f9244eaad60d5aa727db5
laura4rero <lalispfv@gmail.com>
2022-05-17 00:28:11 -0500

    Añdiendo pagina de acualizar datostualizar datos, funcionalidades sin comprobar

---
+++ Frontend/js/actualizarestaurante.js
@@ -0,0 +1,16 @@
+window.onload=()=>
+console.log("hola");
+/*{
+    $('#buttonBuscar').click(()=>
+    console.log ("hola")
+
+    $.ajax({
+        type: 'put',
+        url: 'http://localhost:4000/apiv1ToDo',
+        data: JSON.stringify(infoToSend),
+        contentType: "application/json; charset=utf-8",
+        traditional: true,
+        success: function (data)
+    }
+
+    )};
